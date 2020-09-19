dc46305efc12ed684c5f670d1371e1b71de6c2ea
Vilmar Rodrigues Martins <vilmar.rodrigs@gmail.com>
2020-09-19 08:52:43 -0300

    feat(project-layout): Estruturação do projeto (#1)

---
+++ server/config/express-swagger.js
@@ -0,0 +1,29 @@
+const startup = (app) => {
+  if (process.env.NODE_ENV === 'production') return false
+
+  const expressSwagger = require('express-swagger-generator')(app)
+
+  const options = {
+    swaggerDefinition: {
+      info: {
+        title: 'Caixa Eletrônico',
+        version: '0.0.1',
+        description: 'Documentação - Caixa Eletrônico'
+      },
+      host: `localhost:${process.env.APP_PORT}`,
+      basePath: '/api',
+      produces: ['application/json'],
+      schemes: ['http', 'https']
+    },
+    basedir: __dirname,
+    files: ['../routes/**/*.js'],
+    route: {
+      url: '/api-docs',
+      docs: '/api-docs.json'
+    }
+  }
+
+  expressSwagger(options)
+}
+
+module.exports = { startup }
